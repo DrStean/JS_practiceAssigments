f07892362dd588b76da36affa9d405d890c367fe
DrStein <rene.okt@gmail.com>
2023-03-30 16:45:25 -0400

    First commit

---
+++ challengeCode1.js
@@ -0,0 +1,46 @@
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+*/
+
+
+let marksWeight = 113.4;
+console.log(`Mark's Weight is ${marksWeight} Kilograms`);
+
+let marksHeight = 1.71;
+console.log(`Mark's Height is ${marksHeight} meters`);
+
+let johnsWeight = 100.5;
+console.log(`John's Weight is ${johnsWeight} Kilograms`);
+
+let johnsHeight = 1.9;
+console.log(`John's Height is ${johnsHeight} meters\n \n`);
+
+let marksBMI = (marksWeight / (marksHeight * marksHeight));
+console.log(`The Mark's BMI is ${marksBMI}`);
+
+let johnsBMI = (johnsWeight / (johnsHeight * johnsHeight));
+console.log(`The John's BMI is ${johnsBMI}\n`);
+
+let markHigherBMI = (marksBMI > johnsBMI);
+console.log(markHigherBMI);
+
+if (markHigherBMI = true) {
+    console.log(`Mark's BMI ${marksBMI} is higher than John's BMI ${johnsBMI}`)
+} else {
+    console.log(`John's BMI ${johnsBMI} is higher than Mark's BMI ${marksBMI}`)
+}
+;
